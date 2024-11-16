b1f711eef05d4baccafd380a47639671bc21ead2
JDangar <dangarjignesh9@gmail.com>
2024-11-16 17:28:05 +0530

    First commit

---
+++ src/main/java/com/jdangar/pokemonreview/PokemonreviewApplication.java
@@ -0,0 +1,13 @@
+package com.jdangar.pokemonreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonreviewApplication.class, args);
+	}
+
+}
